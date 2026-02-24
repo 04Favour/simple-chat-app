9e40b4018fd134f211e6f75a1c9526c4b6f9cbec
Favour Mfon <favourmichael004@gmail.com>
2026-02-24 17:58:54 +0100

    add ackee tracker to chat app

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
   }))
   app.useGlobalInterceptors(new TransformInterceptor())
   app.useGlobalFilters(new AllExceptionsFilter())
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
